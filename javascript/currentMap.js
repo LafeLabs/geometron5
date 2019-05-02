deda6c067707852be6ac97ac99cce9e76c8f5007
Lafe Spietz <lafelabs@gmail.com>
2019-05-01 22:41:58 -0400

    map editor is basically working in this newe version, massive simplifiactions of code

--- javascript/currentMap.js
+++ javascript/currentMap.js
@@ -1,93 +1,72 @@
-currentMap = {
-    "name": "other main map",
-    "array": [
-        {
-            "x": 0.07875894988066826,
-            "y": 0.060859188544152745,
-            "w": 0.8770883054892601,
-            "h": 1,
-            "angle": 0,
-            "text": "",
-            "href": "",
-            "url": "http://geometron.000webhostapp.com/uploadimages/image27.jpg",
-            "type": "image"
-        },
-        {
-            "x": 0.36873508353221957,
-            "y": 0.18854415274463007,
-            "w": 0.14916467780429593,
-            "h": 1,
-            "angle": 50,
-            "text": "TOP",
-            "href": "",
-            "url": "",
-            "type": "text"
-        },
-        {
-            "x": 0.1610978520286396,
-            "y": 0.6264916467780429,
-            "w": 0.14916467780429593,
-            "h": 1,
-            "angle": -52,
-            "text": "BOTTOM",
-            "href": "",
-            "url": "",
-            "type": "text"
-        },
-        {
-            "x": 0.25059665871121717,
-            "y": 0.42840095465393796,
-            "w": 0.09904534606205251,
-            "h": 1,
-            "angle": 180,
-            "text": "",
-            "href": "foo/",
-            "url": "http://www.cherrytreemaryland.xyz/symbol/svg/svg1556330629.svg",
-            "type": "image"
-        },
-        {
-            "x": 0.4081145584725537,
-            "y": 0.4212410501193317,
-            "w": 0.09904534606205251,
-            "h": 1,
-            "angle": 267,
-            "text": "",
-            "href": "index.html",
-            "url": "http://www.cherrytreemaryland.xyz/symbol/svg/svg1556330629.svg",
-            "type": "image"
-        },
-        {
-            "x": 0.7088305489260143,
-            "y": 0.6873508353221957,
-            "w": 0.06324582338902147,
-            "h": 1,
-            "angle": 0,
-            "text": "",
-            "href": "",
-            "url": "icons/feedicon.svg",
-            "type": "image"
-        },
-        {
-            "x": 0.5548926014319809,
-            "y": 0.4379474940334129,
-            "w": 0.373508353221957,
-            "h": 1,
-            "angle": 0,
-            "text": "",
-            "href": "",
-            "url": "icons/symbolicon.svg",
-            "type": "image"
-        },
-        {
-            "x": 0.5608591885441527,
-            "y": 0.06443914081145585,
-            "w": 0.3353221957040573,
-            "h": 1,
-            "angle": 0,
-            "text": "",
-            "href": "scroll.html",
-            "url": "icons/scrollicon.svg",
-            "type": "image"
-        }
-    ]
-};
+mapArray = [
+    {
+        "x": 0.07875894988066826,
+        "y": 0.060859188544152745,
+        "w": 0.8770883054892601,
+        "h": 1,
+        "angle": 0,
+        "text": "",
+        "href": "",
+        "src": "http://geometron.000webhostapp.com/uploadimages/image27.jpg"
+    },
+    {
+        "x": 0.3341288782816229,
+        "y": 0.09427207637231504,
+        "w": 0.14916467780429593,
+        "h": 1,
+        "angle": -4,
+        "text": "Some text here",
+        "href": "",
+        "src": ""
+    },
+    {
+        "x": 0.41646778042959426,
+        "y": 0.1431980906921241,
+        "w": 0.09904534606205251,
+        "h": 1,
+        "angle": 180,
+        "text": "",
+        "href": "foo/",
+        "src": "http://www.cherrytreemaryland.xyz/symbol/svg/svg1556330629.svg"
+    },
+    {
+        "x": 0.08472553699284009,
+        "y": 0.6467780429594272,
+        "w": 0.09904534606205251,
+        "h": 1,
+        "angle": 267,
+        "text": "",
+        "href": "index.html",
+        "src": "http://www.cherrytreemaryland.xyz/symbol/svg/svg1556330629.svg"
+    },
+    {
+        "x": 0.7088305489260143,
+        "y": 0.6873508353221957,
+        "w": 0.06324582338902147,
+        "h": 1,
+        "angle": 0,
+        "text": "",
+        "href": "",
+        "src": "icons/feedicon.svg"
+    },
+    {
+        "x": 0.5548926014319809,
+        "y": 0.4379474940334129,
+        "w": 0.373508353221957,
+        "h": 1,
+        "angle": 0,
+        "text": "",
+        "href": "",
+        "src": "icons/symbolicon.svg"
+    },
+    {
+        "x": 0.5608591885441527,
+        "y": 0.06443914081145585,
+        "w": 0.3353221957040573,
+        "h": 1,
+        "angle": 0,
+        "text": "",
+        "href": "scroll.html",
+        "src": "icons/scrollicon.svg"
+    }
+];
--- javascript/mapfactory.js
+++ javascript/mapfactory.js
@@ -1,72 +1,66 @@
 
 
-function MapBox(w,h,map,div) {
+function Map(w,h,div) {
+
     this.w = w;//width of div element map will be drawn in
     this.h = h;//height of div element
-    this.map = map;//Map object, which has a name and an array of MapLinks
     this.div = div; //div element in document
-
     this.div.style.width = this.w + "px"; //set width of div
     this.div.style.height = this.h + "px";//set height of div
-
+    this.array = [];
     this.linkArray = [];
+    //MapLink(x,y,w,angle,text,href,src)
+    var newLink  = new MapLink(0.1,0.1,0.2,0,"text","","");
+    this.array.push(newLink);
 
-    this.draw = function(MapBox) {
-        MapBox.div.innerHTML = "";
-        MapBox.linkArray = [];
-        for(var index = 0;index < MapBox.map.array.length;index++){
+    this.draw = function() {
+        this.div.innerHTML = "";
+        this.linkArray = [];
+        for(var index = 0;index < this.array.length;index++){
             var newa = document.createElement("A");
             newa.style.position = "absolute";
-            MapBox.div.appendChild(newa);
-            newa.style.left = (MapBox.map.array[index].x*MapBox.w).toString() + "px";
-            newa.style.top  = (MapBox.map.array[index].y*MapBox.w).toString() + "px";
-            newa.style.width  = (MapBox.map.array[index].w*MapBox.w).toString() + "px";
-            newa.style.transform  = "rotate(" + (MapBox.map.array[index].angle).toString() + "deg)";
-            if(MapBox.map.array[index].type == "image") {
+            this.div.appendChild(newa);
+            newa.style.left = (this.array[index].x*this.w).toString() + "px";
+            newa.style.top  = (this.array[index].y*this.w).toString() + "px";
+            newa.style.width  = (this.array[index].w*this.w).toString() + "px";
+            newa.style.transform  = "rotate(" + (this.array[index].angle).toString() + "deg)";
+            if(this.array[index].src.length > 0) {
                 var newimg = document.createElement("IMG");
                 newimg.style.position = "absolute";
                 newimg.style.left = "0px";
                 newimg.style.top = "0px";
                 newimg.style.width = "100%";
                 
-                if(MapBox.map.array[index].href.length > 0){
-                    newa.href = MapBox.map.array[index].href;
+                if(this.array[index].href.length > 0){
+                    newa.href = this.array[index].href;
                 }
                 newa.appendChild(newimg);
-                
-                newimg.src = MapBox.map.array[index].url;
-                newimg.alt = MapBox.map.array[index].text;
+                newimg.src = this.array[index].src;
+                newimg.alt = this.array[index].text;
                 newimg.onload = function(){
                     this.parentElement.style.height = this.height + "px";
                 }
             }
-            if(MapBox.map.array[index].type == "text"){
-                //read this https://manishearth.github.io/blog/2017/08/10/font-size-an-unexpectedly-complex-css-property/
-                newa.innerHTML = MapBox.map.array[index].text;
-                newa.style.fontSize = (MapBox.map.array[index].w*MapBox.w/MapBox.map.array[index].text.length).toString() + "px"; 
+            else{
+                newa.innerHTML = this.array[index].text;
+                newa.style.fontSize = (0.1*this.array[index].w*this.w).toString() + "px"; 
             }
-            MapBox.linkArray.push(newa);
+            this.linkArray.push(newa);
         }
     }
 
 }
 
-function Map(name) {
-    this.name = name;
-    this.array = [];
-}
 
-
-function MapLink(x,y,w,h,angle,text,href,url,type) {
+function MapLink(x,y,w,angle,text,href,src) {
+ 
     this.x = x;
     this.y = y;
     this.w = w;
-    this.h = h;
     this.angle = angle;
     this.text = text;
     this.href = href;
-    this.url = url;
-    this.type = type;
+    this.src = src;
 
 }
 
