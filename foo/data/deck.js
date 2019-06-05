c80557644ed219fc5ff0a6e8956655332173ea77
Lafe Spietz <lafelabs@gmail.com>
2019-06-04 20:32:40 -0400

    fixed a sublte bug that made spelling not work in symbol.html

--- foo/data/deck.js
+++
@@ -1,7 +0,0 @@
-deck = [
-    {
-        "name":"home",
-        "url":"http://geometron.000webhostapp.com/geometron5/",
-        "replicator":"https://raw.githubusercontent.com/LafeLabs/geometron5/master/php/replicator.txt"
-    }
-];
